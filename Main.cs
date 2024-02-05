f6a0ec62267f2e70fd9347bdccace7dc85cb4a81
Hermosilla Reinaldo <ghost_cad@hotmail.com>
2024-02-05 00:48:09 -0300

    added up and down controller

--- Main.cs
+++ Main.cs
@@ -3,9 +3,12 @@ using System;
 
 public partial class Main : Node2D
 {
-	private Vector2 _speed = new Vector2(0,6);
+	private Vector2 _speed = new Vector2(0, 6);
 
+	private Vector2 limitUp = new Vector2(-6, -12);
+	private Vector2 limitDown = new Vector2(-6, 6);
 	private Sprite2D _sprite;
+
 	public override void _Ready()
 	{
 		_sprite = GetNode<Sprite2D>("PongPlayer");
@@ -13,10 +16,19 @@ public partial class Main : Node2D
 
 	public override void _Process(double delta)
 	{
-		_sprite.Position += _speed * (float)delta;
-		if(_sprite.Position.Y > 6)
+		movementController(delta);
+	}
+
+	private void movementController(double delta)
+	{
+		// Move as long as the key/button is pressed.
+		if (Input.IsActionPressed("move_up") && _sprite.Position > limitUp)
 		{
-			_sprite.Position = new Vector2(-6,-12);
+			_sprite.Position += -_speed * (float)delta;
+		}
+		if (Input.IsActionPressed("move_down") && _sprite.Position < limitDown)
+		{
+			_sprite.Position += _speed * (float)delta;
 		}
 	}
 }
